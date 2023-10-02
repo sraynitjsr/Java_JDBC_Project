aa5cacfdc60988b2a944a961e56fe3b53aafac01
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-10-02 10:19:19 +0530

    Java Good Old JDBC

---
+++ MyMain.java
@@ -0,0 +1,5 @@
+public class MyMain {
+    public static void main(String args[]) {
+        System.out.println("Hello Java Newbies, Learn JDBC With Me, Stay Updated For Future Code in This Repo, Follow This Repo");
+    }
+}
