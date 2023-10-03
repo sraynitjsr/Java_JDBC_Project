c25f3d60ae30d964d3c0b01a9081c6c099f4f48f
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-10-03 17:57:03 +0530

    JDBC Code

--- MyMain.java
+++ MyMain.java
@@ -1,5 +1,39 @@
+import java.sql.*;
+
 public class MyMain {
-    public static void main(String args[]) {
-        System.out.println("Hello Java Newbies, Learn JDBC With Me, Stay Updated For Future Code in This Repo, Follow This Repo");
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/my_database_name";
+        String username = "my_username";
+        String password = "my_password";
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            connection = DriverManager.getConnection(url, username, password);
+
+            String sqlQuery = "SELECT * FROM my_table_name";
+            preparedStatement = connection.prepareStatement(sqlQuery);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                System.out.println("ID: " + id + ", Name: " + name);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) resultSet.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
